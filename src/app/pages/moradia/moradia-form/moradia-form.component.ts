8b57a12ad81c7e6c27db91f7de1712289531ed7e
nairansilva <nairan.asilva@gmail.com>
2021-09-09 22:14:57 -0300

    Criaçao do formulário moradia

--- src/app/pages/moradia/moradia-form/moradia-form.component.ts
+++ src/app/pages/moradia/moradia-form/moradia-form.component.ts
@@ -1,10 +1,14 @@
-import { Component, Injector, OnDestroy, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { Titular } from './../../titulares/shared/titular.model';
+import { TitularesService } from './../../titulares/shared/titulares.service';
+import { Component, EventEmitter, Injector, OnDestroy, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { PoNotificationService, PoSelectOption, PoComboOption } from '@po-ui/ng-components';
 import { BaseResourceFormComponent } from 'src/app/shared/components/base-resource-form/base-resource-form.component';
 import { OpcoesComboService } from 'src/app/shared/services/opcoes-combo.service';
 import { MoradiaService } from '../shared/moradia.service';
 import { Moradias } from '../shared/moradias.model';
+import { Subscription } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-moradia-form',
@@ -13,9 +17,14 @@ import { Moradias } from '../shared/moradias.model';
 })
 export class MoradiaFormComponent extends BaseResourceFormComponent<Moradias> implements OnDestroy {
 
+  private subscriptionFormularioMoradia: Subscription;
+  private telaIniciada: boolean = false;
+
+  private dadosTitular: Titular;
+
   public formularioMoradia: FormGroup;
 
-  public familiaTemCadastroUnico: Array<PoSelectOption>;
+  public familiaTemCadastroUnicoOpcoes: Array<PoSelectOption>;
   public boleanoOpcoes: Array<PoSelectOption>;
   public moradiaOpcoes: Array<PoSelectOption>;
   public quantidadeBanheirosOpcoes: Array<PoSelectOption>;
@@ -29,14 +38,18 @@ export class MoradiaFormComponent extends BaseResourceFormComponent<Moradias> im
   public desastresOpcoes: Array<PoSelectOption>;
   public usoMoradiaOpcoes: Array<PoSelectOption>;
 
+  @Output() formularioMoradiaValido: EventEmitter<FormGroup> = new EventEmitter()
 
-  constructor(    protected injector: Injector,
+
+  constructor(protected injector: Injector,
     private poNotificationService: PoNotificationService,
     private opcoesComboService: OpcoesComboService,
     private moradiaService: MoradiaService,
+    private titularesService: TitularesService,
     private fb: FormBuilder,
   ) {
-    super(injector, new Moradias(), moradiaService)
+    super(injector, new Moradias(), moradiaService);
+    this.dadosTitular = JSON.parse(this.titularesService.getTitularInfo());
   }
 
   protected buildResourceForm(): void { }
@@ -51,15 +64,16 @@ export class MoradiaFormComponent extends BaseResourceFormComponent<Moradias> im
 
   ngOnInit(): void {
     this.initialize();
+    this.criaFormulario()
   }
 
   initialize(): void {
-    this.familiaTemCadastroUnico = this.opcoesComboService.familiaTemCadastroUnicoOpcoes;
+    this.familiaTemCadastroUnicoOpcoes = this.opcoesComboService.familiaTemCadastroUnicoOpcoes;
     this.boleanoOpcoes = this.opcoesComboService.boleanoOpcoes;
     this.moradiaOpcoes = this.opcoesComboService.moradiaOpcoes;
     this.quantidadeBanheirosOpcoes = this.opcoesComboService.quantidadeBanheirosOpcoes;
     this.quantidadeComodosOpcoes = this.opcoesComboService.quantidadeComodosOpcoes;
-    this.tipoMoradiaOpcoes  = this.opcoesComboService.tipoMoradiaOpcoes;
+    this.tipoMoradiaOpcoes = this.opcoesComboService.tipoMoradiaOpcoes;
     this.caracteristicasCasaOpcoes = this.opcoesComboService.caracteristicasCasaOpcoes;
     this.acessoEnergiaOpcoes = this.opcoesComboService.acessoEnergiaOpcoes;
     this.acessoAguaOpcoes = this.opcoesComboService.acessoAguaOpcoes;
@@ -70,7 +84,56 @@ export class MoradiaFormComponent extends BaseResourceFormComponent<Moradias> im
 
   }
 
-  ngOnDestroy(){
+  criaFormulario(): void {
+    this.formularioMoradia = this.fb.group({
+      assentamento: ['-', Validators.compose([Validators.required])],
+      numeroSelagemAtual: ['', Validators.compose([Validators.required])],
+      numeroSelagemAntiga: ['', Validators.compose([Validators.required])],
+      endereco: ['', Validators.compose([Validators.required])],
+      numero: ['', Validators.compose([Validators.required])],
+      complemento: ['', Validators.compose([Validators.required])],
+      bairro: ['', Validators.compose([Validators.required])],
+      familiaTemCadastroUnico: ['', Validators.compose([Validators.required])],
+      familiaAcessaUnidadeBasicaSaude: ['', Validators.compose([Validators.required])],
+      qualUnidadeBasicaSaude: ['', Validators.compose([Validators.required])],
+      familiaAcessoEscolas: ['', Validators.compose([Validators.required])],
+      utilizaTransporteEscolar: ['', Validators.compose([Validators.required])],
+      familiaAcessaCRAS: ['', Validators.compose([Validators.required])],
+      familiaAcessaCREAS: ['', Validators.compose([Validators.required])],
+      familiaAcessaServicosConvivenciaCrianca: ['', Validators.compose([Validators.required])],
+      familiaAcessaServicosConvivenciaIdosos: ['', Validators.compose([Validators.required])],
+      moradia: ['', Validators.compose([Validators.required])],
+      quantidadeBanheiros: ['', Validators.compose([Validators.required])],
+      quantidadeComodos: ['', Validators.compose([Validators.required])],
+      tipoMoradiaOcupada: ['', Validators.compose([Validators.required])],
+      caracteristicasCasa: ['', Validators.compose([Validators.required])],
+      acessoEnergiaEletrica: ['', Validators.compose([Validators.required])],
+      acessoAbastecimentoAgua: ['', Validators.compose([Validators.required])],
+      acessoSaneamentoSanitario: ['', Validators.compose([Validators.required])],
+      possuiAutomoveis: ['', Validators.compose([Validators.required])],
+      moradiaSofreuDesastre: ['', Validators.compose([Validators.required])],
+      usoMoradia: ['', Validators.compose([Validators.required])],
+      cachorro: [''],
+      gato: [''],
+      passaro: [''],
+      outroAnimal: [''],
+      energiaEletrica: ['', Validators.compose([Validators.required])],
+      aguaEsgoto: ['', Validators.compose([Validators.required])],
+      gastoAlimentacao: [''],
+      gastoMedicamentos: [''],
+      totalDespesas: [''],
+      observacoes: [''],
 
+    });
+
+    this.subscriptionFormularioMoradia = this.formularioMoradia.valueChanges.pipe(
+      debounceTime(1000)
+    ).subscribe(res => this.telaIniciada ? this.formularioMoradiaValido.emit(this.formularioMoradia) : this.telaIniciada = true)
+
+    // , Validators.compose([Validators.required])
+  }
+
+  ngOnDestroy() {
+    this.subscriptionFormularioMoradia.unsubscribe();
   }
 }
