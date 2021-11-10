494fb88b4d6da9ab2840d284aa62e574a81ef089
nairansilva <nairan.asilva@gmail.com>
2021-11-09 23:00:01 -0300

    Ajuste na máscara da moeda

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule,LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -10,6 +10,13 @@ import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AuthTokenInterceptor } from './shared/internceptor/auth-token.interceptor';
 import { LoginModule } from './core/login/shared/login.module';
+
+// **************************************************
+import ptBr from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(ptBr);
+// **************************************************
 
 @NgModule({
   declarations: [
@@ -25,7 +32,8 @@ import { LoginModule } from './core/login/shared/login.module';
     LoginModule
   ],
   providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: AuthTokenInterceptor, multi: true }
+    { provide: HTTP_INTERCEPTORS, useClass: AuthTokenInterceptor, multi: true },
+    {provide: LOCALE_ID, useValue: 'pt'}
   ],
   bootstrap: [AppComponent]
 })
