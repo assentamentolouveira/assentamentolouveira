bfc42d8a692bcf755f5af8c83b533e12f505b67f
nairansilva <nairan.asilva@gmail.com>
2022-04-06 22:22:26 -0300

    Projeto Inicial de Produção

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: true,
   imagesPath: 'assets/img/',
   // URL: "https://pref-louveira-assentamento.azurewebsites.net/api/v1",
-  URL: "https://assentamento-louveira-dev.azurewebsites.net/api/v1",
+  URL: "https://simhab-louveira.azurewebsites.net/api/v1",
   URLCartaoCidadao: "https://webservice.assistsistemas.com.br/blank_ws_exporta/?user=incidade&pass=NZwdRiQR",
   // URLFilter: "https://pref-louveira-assentamento.azurewebsites.net/odata/"
-  URLFilter: "https://assentamento-louveira-dev.azurewebsites.net/odata/"
+  URLFilter: "https://simhab-louveira.azurewebsites.net/odata/"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,10 +6,10 @@ export const environment = {
   production: false,
   imagesPath: 'assets/img/',
   // URL: "https://pref-louveira-assentamento.azurewebsites.net/api/v1",
-  URL: "https://simhab-dev-louveira.azurewebsites.net/api/v1",
+  URL: "https://simhab-louveira.azurewebsites.net/api/v1",
   URLCartaoCidadao: "https://webservice.assistsistemas.com.br/blank_ws_exporta/?user=incidade&pass=NZwdRiQR",
   // URLFilter: "https://pref-louveira-assentamento.azurewebsites.net/odata/"
-  URLFilter: "https://simhab-dev-louveira.azurewebsites.net/odata/"
+  URLFilter: "https://simhab-louveira.azurewebsites.net/odata/"
 
 };
 
