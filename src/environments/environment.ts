cd05648cf5b4d7dd5b87ffb02a8a0abb2c622f67
nairansilva <nairan.asilva@gmail.com>
2022-04-06 21:32:16 -0300

    Projeto da virada

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,10 +6,10 @@ export const environment = {
   production: false,
   imagesPath: 'assets/img/',
   // URL: "https://pref-louveira-assentamento.azurewebsites.net/api/v1",
-  URL: "https://assentamento-louveira-dev.azurewebsites.net/api/v1",
+  URL: "https://simhab-dev-louveira.azurewebsites.net/api/v1",
   URLCartaoCidadao: "https://webservice.assistsistemas.com.br/blank_ws_exporta/?user=incidade&pass=NZwdRiQR",
   // URLFilter: "https://pref-louveira-assentamento.azurewebsites.net/odata/"
-  URLFilter: "https://assentamento-louveira-dev.azurewebsites.net/odata/"
+  URLFilter: "https://simhab-dev-louveira.azurewebsites.net/odata/"
 
 };
 
